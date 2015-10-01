17960167f478ac78d89df2d4a2fda9e7c968454c
Sebastian Sandqvist <seb@sparque.me>
2015-10-01 22:48:17 +0700

    Close #2 PR: Allow 0 and negative precision.

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ test('roundTo()', function (t) {
 	t.assert(fn(0.129, 2) === 0.13);
 	t.assert(fn(0.129, 1) === 0.1);
 	t.assert(fn(1.005, 2) === 1.01);
+	t.assert(fn(1.005, 0) === 1);
+	t.assert(fn(111.1, -2) === 100);
 	t.end();
 });
 
@@ -15,6 +17,8 @@ test('roundTo.up()', function (t) {
 	t.assert(fn.up(0.111, 2) === 0.12);
 	t.assert(fn.up(0.111, 1) === 0.2);
 	t.assert(fn.up(1.004, 2) === 1.01);
+	t.assert(fn.up(1.111, 0) === 2);
+	t.assert(fn.up(111.1, -2) === 200);
 	t.end();
 });
 
@@ -23,5 +27,7 @@ test('roundTo.down()', function (t) {
 	t.assert(fn.down(0.666, 2) === 0.66);
 	t.assert(fn.down(0.666, 1) === 0.6);
 	t.assert(fn.down(1.006, 2) === 1.00);
+	t.assert(fn.down(1.006, 0) === 1);
+	t.assert(fn.down(111.6, -2) === 100);
 	t.end();
 });
