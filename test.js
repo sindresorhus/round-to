cb39653006cea0aa8ea21f554a00b6408d466d2d
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:44:49 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,33 +1,29 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import fn from './';
 
-test('roundTo()', function (t) {
-	t.assert(fn(0.129, 3) === 0.129);
-	t.assert(fn(0.129, 2) === 0.13);
-	t.assert(fn(0.129, 1) === 0.1);
-	t.assert(fn(1.005, 2) === 1.01);
-	t.assert(fn(1.005, 0) === 1);
-	t.assert(fn(111.1, -2) === 100);
-	t.end();
+test('roundTo()', t => {
+	t.is(fn(0.129, 3), 0.129);
+	t.is(fn(0.129, 2), 0.13);
+	t.is(fn(0.129, 1), 0.1);
+	t.is(fn(1.005, 2), 1.01);
+	t.is(fn(1.005, 0), 1);
+	t.is(fn(111.1, -2), 100);
 });
 
-test('roundTo.up()', function (t) {
-	t.assert(fn.up(0.111, 3) === 0.111);
-	t.assert(fn.up(0.111, 2) === 0.12);
-	t.assert(fn.up(0.111, 1) === 0.2);
-	t.assert(fn.up(1.004, 2) === 1.01);
-	t.assert(fn.up(1.111, 0) === 2);
-	t.assert(fn.up(111.1, -2) === 200);
-	t.end();
+test('roundTo.up()', t => {
+	t.is(fn.up(0.111, 3), 0.111);
+	t.is(fn.up(0.111, 2), 0.12);
+	t.is(fn.up(0.111, 1), 0.2);
+	t.is(fn.up(1.004, 2), 1.01);
+	t.is(fn.up(1.111, 0), 2);
+	t.is(fn.up(111.1, -2), 200);
 });
 
-test('roundTo.down()', function (t) {
-	t.assert(fn.down(0.666, 3) === 0.666);
-	t.assert(fn.down(0.666, 2) === 0.66);
-	t.assert(fn.down(0.666, 1) === 0.6);
-	t.assert(fn.down(1.006, 2) === 1.00);
-	t.assert(fn.down(1.006, 0) === 1);
-	t.assert(fn.down(111.6, -2) === 100);
-	t.end();
+test('roundTo.down()', t => {
+	t.is(fn.down(0.666, 3), 0.666);
+	t.is(fn.down(0.666, 2), 0.66);
+	t.is(fn.down(0.666, 1), 0.6);
+	t.is(fn.down(1.006, 2), 1.00);
+	t.is(fn.down(1.006, 0), 1);
+	t.is(fn.down(111.6, -2), 100);
 });
