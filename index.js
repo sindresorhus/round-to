ffbefa544dbdca679d0aab529e6e7d3366308774
amrali-eg <32075105+amrali-eg@users.noreply.github.com>
2020-10-14 21:13:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,9 @@ function round(method, number, precision) {
 		number = Math.abs(number);
 	}
 
-	let exponent;
-	[number, exponent] = `${number}e`.split('e');
-	let result = Math[method](`${number}e${Number(exponent) + precision}`);
+	let power = Math.pow(10, precision);
 
-	[number, exponent] = `${result}e`.split('e');
-	result = Number(`${number}e${Number(exponent) - precision}`);
+	let result = Math[method](+(number * power).toPrecision(15)) / power;
 
 	if (isRoundingAndNegative) {
 		result = -result;
