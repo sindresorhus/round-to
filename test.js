dbf325458d1d3fa9ff6048186806a85575b8a35c
amrali-eg <32075105+amrali-eg@users.noreply.github.com>
2020-11-07 07:30:48 +0300

    Update test.js

--- test.js
+++ test.js
@@ -12,6 +12,16 @@ test('roundTo()', t => {
 	t.false(Number.isNaN(roundTo(10000000000000, 8)));
 	t.is(roundTo(0.37542323423423432432432432432, 8), 0.37542323); // eslint-disable-line no-loss-of-precision
 	t.is(roundTo(0.1231782638, Infinity), 0.1231782638);
+	t.is(roundTo(0.5, 0), 1);
+	t.is(roundTo(-0.5, 0), -1);
+	t.is(roundTo(5.12, 1), 5.1);
+	t.is(roundTo(-5.12, 1), -5.1);
+	t.is(roundTo(1.005, 2), 1.01);
+	t.is(roundTo(-1.005, 2), -1.01);
+	t.is(roundTo(39.425, 2), 39.43);
+	t.is(roundTo(-39.425, 2), -39.43);
+	t.is(roundTo(1262.48, -1), 1260);
+	t.is(roundTo(1262.48, -2), 1300);
 	t.is(roundTo(0.597 / 6, 3), 0.1);
 });
 
@@ -23,6 +33,13 @@ test('roundTo.up()', t => {
 	t.is(roundTo.up(1.111, 0), 2);
 	t.is(roundTo.up(111.1, -2), 200);
 	t.is(roundTo.up(-0.375, 2), -0.37);
+	t.is(roundTo.up(1e-8, 2), 0.01);
+	t.is(roundTo.up(5.12, 1), 5.2);
+	t.is(roundTo.up(-5.12, 1), -5.1);
+	t.is(roundTo.up(9.130, 2), 9.13);
+	t.is(roundTo.up(65.180, 2), 65.18);
+	t.is(roundTo.up(-2.260, 2), -2.26);
+	t.is(roundTo.up(-18.150, 2), -18.15);
 	t.is(roundTo.up((0.1 + 0.2) * 10, 0), 3);
 });
 
@@ -34,5 +51,12 @@ test('roundTo.down()', t => {
 	t.is(roundTo.down(1.006, 0), 1);
 	t.is(roundTo.down(111.6, -2), 100);
 	t.is(roundTo.down(-0.375, 2), -0.38);
+	t.is(roundTo.down(1e-8, 2), 0);
+	t.is(roundTo.down(5.12, 1), 5.1);
+	t.is(roundTo.down(-5.12, 1), -5.2);
+	t.is(roundTo.down(-9.130, 2), -9.13);
+	t.is(roundTo.down(-65.180, 2), -65.18);
+	t.is(roundTo.down(2.260, 2), 2.26);
+	t.is(roundTo.down(18.150, 2), 18.15);
 	t.is(roundTo.down((0.1 + 0.7) * 10, 0), 8);
 });
